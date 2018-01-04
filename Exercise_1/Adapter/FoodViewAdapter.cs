49415df28142ff04e70012be8b4761829035edde
Mẫn <vanman.lqd@gmail.com>
2018-01-04 22:22:26 +0700

    Add exercise 1

---
+++ Exercise_1/Adapter/FoodViewAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Exercise_1.Models;
+
+namespace Exercise_1.Adapter
+{
+    public class FoodViewAdapter : RecyclerView.Adapter
+    {
+        public ListFood Foods { get; }
+
+        public FoodViewAdapter(ListFood foods)
+        {
+            Foods = foods;
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            var foodViewHolder = holder as FoodViewHolder;
+            foodViewHolder.TextViewCost.Text = Foods[position].FoodCost;
+            foodViewHolder.TextViewFood.Text = Foods[position].FoodName;
+
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            var itemView = LayoutInflater.From(parent.Context).
+                Inflate(Resource.Layout.food_view_item, parent, false);
+            return new FoodViewHolder(itemView);
+        }
+
+        public override int ItemCount => Foods.NumOfFoods;
+    }
+}
---
+++ Exercise_1/Models/ListFood.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+using Newtonsoft.Json;
+
+namespace Exercise_1.Models
+{
+    
+    public class ListFood
+    {
+        private readonly List<Food> foods;
+
+        public ListFood()
+        {
+            foods = BuildInFoods;
+        }
+
+        private static readonly List<Food> BuildInFoods = new List<Food>()
+        {
+            new Food("Hamburger", "10$", "iamge"),
+            new Food("Chicken", "20$", "iamge")
+        };
+        public int NumOfFoods => foods.Count;
+        public Food this[int i] => foods[i];
+    }
+
+}
