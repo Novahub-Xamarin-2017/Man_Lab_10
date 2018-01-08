b5cd04a655eaf795181ac25c94420e98b4172628
Mẫn <vanman.lqd@gmail.com>
2018-01-08 15:58:41 +0700

    Add code

---
+++ Exercise_3/Adapters/FileAdapter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Exercise_3.Interfaces;
+
+namespace Exercise_3.Adapters
+{
+    public class FileAdapter : RecyclerView.Adapter, IItemClickListener
+    {
+
+        public List<string> Files { get; }
+        private Context context;
+
+        public FileAdapter(List<string> files, Context context)
+        {
+            Files = files;
+            this.context = context;
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            if (holder is ItemViewHolder viewholder)
+            {
+                viewholder.TvName.Text = Files[position];
+                viewholder.SetOnItemClickListener(this);
+            }
+        }
+
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            var itemView = LayoutInflater.From(parent.Context)
+                .Inflate(Resource.Layout.recyclerview_item, parent, false);
+            return new ItemViewHolder(itemView);
+        }
+
+        public override int ItemCount => Files.Count;
+        public void OnClick(View itemView, int position, bool isLongClick)
+        {
+            if (isLongClick)
+            {
+                Toast.MakeText(context, $"Long click : {Files[position]}", ToastLength.Short).Show();
+            }
+            else
+            {
+                Toast.MakeText(context, $"{Files[position]}", ToastLength.Short).Show();
+            }
+        }
+    }
+}
---
+++ Exercise_3/Adapters/ItemViewHolder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Exercise_3.Interfaces;
+
+namespace Exercise_3.Adapters
+{
+    public class ItemViewHolder : RecyclerView.ViewHolder, View.IOnClickListener, View.IOnLongClickListener
+    {
+        [InjectView(Resource.Id.txtName)] public TextView TvName;
+        private IItemClickListener itemClickListener;
+        public ItemViewHolder(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public ItemViewHolder(View itemView) : base(itemView)
+        {
+            Cheeseknife.Inject(this, itemView);
+            itemView.SetOnClickListener(this);
+            itemView.SetOnLongClickListener(this);
+        }
+
+        public void SetOnItemClickListener(IItemClickListener itemClickListener)
+        {
+            this.itemClickListener = itemClickListener;
+        }
+
+        public void OnClick(View v)
+        {
+            itemClickListener.OnClick(v, AdapterPosition, false);
+        }
+
+        public bool OnLongClick(View v)
+        {
+            itemClickListener.OnClick(v, AdapterPosition, true);
+            return true;
+        }
+    }
+}
