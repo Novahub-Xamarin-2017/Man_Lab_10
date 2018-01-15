c44c5c1e54897b6e20fa57e4efc07aca385a8689
Mẫn <vanman.lqd@gmail.com>
2018-01-15 09:48:36 +0700

    add code

---
+++ Exercise_4/Adapters/ViewPagerAdapter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using Fragment = Android.Support.V4.App.Fragment;
+using FragmentManager = Android.Support.V4.App.FragmentManager;
+
+namespace Exercise_4.Adapters
+{
+    public class ViewPagerAdapter : FragmentPagerAdapter
+    {
+        private List<Fragment> fragments = new List<Fragment>();
+
+        private List<string> titles = new List<string>();
+
+        public ViewPagerAdapter(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        public ViewPagerAdapter(FragmentManager fm) : base(fm)
+        {
+        }
+
+        public override int Count => fragments.Count;
+        public override Fragment GetItem(int position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Exercise_4/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+
+namespace Exercise_4
+{
+    [Activity(Label = "Exercise_4", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            var viewPager = FindViewById<ViewPager>(Resource.Id.viewPager);
+        }
+    }
+}
+
